cd58a65e0bf965bd6a6b70990fa9e8336776dbdd
K.Meshkov <skyline@mail.ru>
2020-09-04 11:40:03 +0300

    feat: add open-close functionality to form

---
+++ script.js
@@ -0,0 +1,21 @@
+const editButton = document.querySelector('.edit-button');
+
+const popup = document.querySelector('.popup');
+const popup_close = popup.querySelector('.popup_close');
+const saveButton = popup.querySelector('.save-button');
+
+
+const profile = document.querySelector('.profile__text');
+let authorName = profile.querySelector('.author__name');
+let authorInfo = profile.querySelector('.author__info');
+
+function togglePopup() {
+    popup.classList.toggle('popup_hidden');
+}
+
+console.log({editButton, popup, popup_close, saveButton, authorName, authorInfo});
+
+
+editButton.addEventListener('click', togglePopup);
+popup_close.addEventListener('click', togglePopup);
+
--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-const page = document.querySelector('.page');
-/*const container = page.querySelector('.page__container');*/
-const popup = page.querySelector('.popup');
-
-console.log(popup);

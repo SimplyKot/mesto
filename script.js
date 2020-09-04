f2715f6a7c7828e8a026f35b42e48a2df6d0ce29
K.Meshkov <skyline@mail.ru>
2020-09-04 13:21:42 +0300

    feat: change profile values from form

--- script.js
+++ script.js
@@ -3,27 +3,25 @@ const editButton = document.querySelector('.edit-button');
 const popup = document.querySelector('.popup');
 const popup_close = popup.querySelector('.popup_close');
 const saveButton = popup.querySelector('.save-button');
+const inputFields = popup.querySelectorAll('.popup__field');
 
 
 const profile = document.querySelector('.profile__text');
-let authorName = profile.querySelector('.author__name');
-let authorInfo = profile.querySelector('.author__info');
-
-
-console.log(authorInfo);
-
+const authorName = profile.querySelector('.author__name');
+const authorInfo = profile.querySelector('.author__info');
 
 function togglePopup() {
     popup.classList.toggle('popup_hidden');
+    inputFields[0].value = authorName.textContent;;
+    inputFields[1].value = authorInfo.textContent;
 }
 
-
-
-
-
-console.log({editButton, popup, popup_close, saveButton, authorName, authorInfo});
-
+function saveAndClose () {
+    authorName.textContent = inputFields[0].value;
+    authorInfo.textContent = inputFields[1].value;
+    togglePopup();
+}
 
 editButton.addEventListener('click', togglePopup);
 popup_close.addEventListener('click', togglePopup);
-
+saveButton.addEventListener('click', saveAndClose);

17646af37a9be5fbe1e2964745526585571d03d0
K.Meshkov <skyline@mail.ru>
2020-09-04 14:07:49 +0300

    feat: add submit button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const editButton = document.querySelector('.edit-button');
 
 const popup = document.querySelector('.popup');
+const popup_form = popup.querySelector('.popup__content')
 const popup_close = popup.querySelector('.popup_close');
 const saveButton = popup.querySelector('.save-button');
 const inputFields = popup.querySelectorAll('.popup__field');
@@ -10,18 +11,21 @@ const profile = document.querySelector('.profile__text');
 const authorName = profile.querySelector('.author__name');
 const authorInfo = profile.querySelector('.author__info');
 
+
 function togglePopup() {
     popup.classList.toggle('popup_hidden');
-    inputFields[0].value = authorName.textContent;;
+    inputFields[0].value = authorName.textContent;
     inputFields[1].value = authorInfo.textContent;
 }
 
-function saveAndClose () {
-    authorName.textContent = inputFields[0].value;
-    authorInfo.textContent = inputFields[1].value;
-    togglePopup();
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    let nameInput = inputFields[0].value;
+    let jobInput = inputFields[1].value;
+    authorName.textContent = nameInput;
+    authorInfo.textContent = jobInput;
 }
 
 editButton.addEventListener('click', togglePopup);
 popup_close.addEventListener('click', togglePopup);
-saveButton.addEventListener('click', saveAndClose);
+popup_form.addEventListener('submit', formSubmitHandler);

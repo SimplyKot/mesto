f4fada92a174aaf2bbfb2b434ce2f925815f5ba6
K.Meshkov <skyline@mail.ru>
2020-09-30 16:02:11 +0300

    feat: add Escape key behavior

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,28 @@
 const initialCards = [
-    {
-        name: 'Москва',
-        link: 'https://images.unsplash.com/photo-1599849579035-13e3e19db29f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=50'
-    },
-    {
-        name: 'Москва-река',
-        link: 'https://images.unsplash.com/photo-1523509433743-6f42a58221df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=80'
-    },  
-    {
-      name: 'Владивосток',
-      link: 'https://images.unsplash.com/photo-1563941433-b6a094653ed2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=374&q=80'
-    },
-    {
-        name: 'Парк Паанаярви',
-        link: 'https://images.unsplash.com/photo-1568230315894-1edd16d248b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=80'
-    },
-    {
-        name: 'Никола-Ленивец',
-        link: 'https://images.unsplash.com/photo-1561919430-54877acf8107?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=80'
-    },
-    {
-      name: 'Онежское озеро',
-      link: 'https://images.unsplash.com/photo-1543699936-c901ddbf0c05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=80'
-    }
+  {
+    name: 'Москва',
+    link: 'https://images.unsplash.com/photo-1599849579035-13e3e19db29f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=50'
+  },
+  {
+    name: 'Москва-река',
+    link: 'https://images.unsplash.com/photo-1523509433743-6f42a58221df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=80'
+  },
+  {
+    name: 'Владивосток',
+    link: 'https://images.unsplash.com/photo-1563941433-b6a094653ed2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=374&q=80'
+  },
+  {
+    name: 'Парк Паанаярви',
+    link: 'https://images.unsplash.com/photo-1568230315894-1edd16d248b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=80'
+  },
+  {
+    name: 'Никола-Ленивец',
+    link: 'https://images.unsplash.com/photo-1561919430-54877acf8107?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=80'
+  },
+  {
+    name: 'Онежское озеро',
+    link: 'https://images.unsplash.com/photo-1543699936-c901ddbf0c05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=80'
+  }
 ];
 
 const editButton = document.querySelector('.profile__edit-button');
@@ -52,30 +52,30 @@ const popupImageTitle = popupImage.querySelector('.popup-image__image-title');
 
 
 function togglePopup() {
-    if (!popup.classList.contains('popup_opened')) {
-        inputNameField.value = authorName.textContent;
-        inputInfoField.value = authorInfo.textContent;
-    }
-        popup.classList.toggle('popup_opened');
+  if (!popup.classList.contains('popup_opened')) {
+    inputNameField.value = authorName.textContent;
+    inputInfoField.value = authorInfo.textContent;
+  }
+  popup.classList.toggle('popup_opened');
 }
 
 function togglePopupAdd() {
-    popupAdd.classList.toggle('popup-add_opened');
+  popupAdd.classList.toggle('popup-add_opened');
 }
 
 function togglePopupImage() {
-    popupImage.classList.toggle('popup-image_opened');
+  popupImage.classList.toggle('popup-image_opened');
 }
 
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    authorName.textContent = inputNameField.value;
-    authorInfo.textContent = inputInfoField.value;
-    togglePopup();
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  authorName.textContent = inputNameField.value;
+  authorInfo.textContent = inputInfoField.value;
+  togglePopup();
 }
 
-function formSubmitPlaceHandler (evt) {
+function formSubmitPlaceHandler(evt) {
   const submitButton = evt.target.querySelector('.popup__button');
   evt.preventDefault();
   const newCard = {};
@@ -93,32 +93,32 @@ function formSubmitPlaceHandler (evt) {
 Любоу другое значение - в конец
 */
 
-function addCard(card, position='end') {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.cloneNode(true);
-    const cardImage = cardElement.querySelector('.card__image');
-  
-    cardElement.querySelector('.card__delete-button').addEventListener('click',  evt => evt.target.closest('.card').remove());
-    cardImage.setAttribute('src',card.link);
-    cardImage.setAttribute('alt',card.name);
-  
-    cardImage.addEventListener('click', () => {
- 
-      popupImagePicture.setAttribute('src',card.link);
-      popupImagePicture.setAttribute('alt',card.name);
-      popupImageTitle.textContent=card.name;
-      togglePopupImage();
-    });
-  
-    cardElement.querySelector('.card__name').textContent=card.name;
-    cardElement.querySelector('.card__like-button').addEventListener('click', evt => evt.target.classList.toggle('card__like-button_active'));
-    if (position != 'start') {
-      cardsList.append(cardElement);
-    }
-    else {
-      cardsList.prepend(cardElement);
-    }
-   
+function addCard(card, position = 'end') {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+
+  cardElement.querySelector('.card__delete-button').addEventListener('click', evt => evt.target.closest('.card').remove());
+  cardImage.setAttribute('src', card.link);
+  cardImage.setAttribute('alt', card.name);
+
+  cardImage.addEventListener('click', () => {
+
+    popupImagePicture.setAttribute('src', card.link);
+    popupImagePicture.setAttribute('alt', card.name);
+    popupImageTitle.textContent = card.name;
+    togglePopupImage();
+  });
+
+  cardElement.querySelector('.card__name').textContent = card.name;
+  cardElement.querySelector('.card__like-button').addEventListener('click', evt => evt.target.classList.toggle('card__like-button_active'));
+  if (position != 'start') {
+    cardsList.append(cardElement);
+  }
+  else {
+    cardsList.prepend(cardElement);
+  }
+
 }
 
 function renderCardList() {
@@ -130,9 +130,23 @@ popupCloseButton.addEventListener('click', togglePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
 
 addButton.addEventListener('click', togglePopupAdd);
-popupAddCloseButton.addEventListener('click',togglePopupAdd);
-popupAddForm.addEventListener('submit',formSubmitPlaceHandler);
+popupAddCloseButton.addEventListener('click', togglePopupAdd);
+popupAddForm.addEventListener('submit', formSubmitPlaceHandler);
 
-popupImage_close.addEventListener('click',togglePopupImage);
+popupImage_close.addEventListener('click', togglePopupImage);
+
+document.addEventListener('keydown', evt => {
+  if (evt.key === "Escape") {
+    if (popup.classList.contains('popup_opened')) {
+      togglePopup();
+    }
+    if (popupAdd.classList.contains('popup-add_opened')) {
+      togglePopupAdd();
+    }
+    if (popupImage.classList.contains('popup-image_opened')) {
+      togglePopupImage();
+    }
+  }
+});
 
 renderCardList();

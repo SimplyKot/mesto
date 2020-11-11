d75738fd917b0bff2f02ca6fcb3b1f4d5169d190
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2020-11-12 01:46:32 +0300

    feat: api in Userindo experiments

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,15 +1,26 @@
 export default class UserInfo {
-  constructor({name,about,avatar = ''}) {
+  constructor({name,about,avatar = ''},api) {
     this._name = name;
     this._info = about;
     this._avatar = avatar;
+    this.api = api;
+  }
+
+  _updateData(newData) {
+      this._name = newData.name;
+      this._info = newData.about;
+      this._avatar = newData.avatar;
   }
 
   getUserInfo() {
-    const result = {name:this._name,about:this._info,avatar:this._avatar};
-    //console.log('result:', result);
-    return result;
-  }
+    this.api.getUserInfo().then((data)=>
+    {this._updateData({name:data.name, about:data.about, avatar: data.avatar})
+      return data;
+    });
+
+    return {name:this._name,about:this._info,avatar:this._avatar};
+    }
+
 
   setUserInfo({name,about}) {
     //console.log(name, about, avatar);

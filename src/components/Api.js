37a8232e56a367098f514a65466735cdd9c90b56
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2020-11-15 13:18:27 +0300

    fix: redublicate code in Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,132 +4,71 @@ export default class Api {
     this._headers = headers;
   }
 
-  getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
-    })
+  _transmit(url, method, body = "") {
+    const localParam = { method: method, headers: this._headers };
+    if (body) {
+      localParam.body = JSON.stringify(body);
+    }
+
+    return fetch(url, localParam)
       .then((res) => {
         if (!res.ok) {
-          return Promise.reject("Server error");
+          return Promise.reject(
+            `Ошибка сервера (${res.status}): ${res.statusText}`
+          );
         }
         return res.json();
-      })
-      .then((data) => {
-        return data;
       })
       .catch((err) => {
         console.log(err);
       });
+  }
+
+  getInitialCards() {
+    // `${this._baseUrl}/cards` GET
+    return this._transmit(`${this._baseUrl}/cards`, "GET");
   }
 
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers,
-    })
-      .then((res) => {
-        if (!res.ok) {
-          return Promise.reject("Server error");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        return data;
-      })
-      .catch((err) => console.log(err));
+    // `${this._baseUrl}/users/me` GET
+    return this._transmit(`${this._baseUrl}/users/me`, "GET");
   }
 
   editProfile(data) {
-    return fetch(`${this._baseUrl}/users/me`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        name: data.name,
-        about: data.about,
-      }),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          return Promise.reject("Server error");
-        }
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // `${this._baseUrl}/users/me` PATCH
+    return this._transmit(`${this._baseUrl}/users/me`, "PATCH", {
+      name: data.name,
+      about: data.about,
+    });
   }
 
   addCard(data) {
-    //console.log(data);
-    return fetch(`${this._baseUrl}/cards`, {
-      method: "POST",
-      headers: this._headers,
-      body: JSON.stringify({
-        name: data.name,
-        link: data.link,
-        likes: data.likes,
-      }),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          return Promise.reject("Server error");
-        }
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // `${this._baseUrl}/cards POST
+    return this._transmit(`${this._baseUrl}/cards`, "POST", {
+      name: data.name,
+      link: data.link,
+      likes: data.likes,
+    });
   }
 
   deleteCard(id) {
-    return fetch(`${this._baseUrl}/cards/${id}`, {
-      method: "DELETE",
-      headers: this._headers,
-    })
-      .then((res) => {
-        if (!res.ok) {
-          return Promise.reject("Server error");
-        }
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // `${this._baseUrl}/cards/${id}` DELETE
+    return this._transmit(`${this._baseUrl}/cards/${id}`, "DELETE");
   }
 
   // Атрибут action со значением DELETE снимает лайк
   setLike(id, action = "") {
-    return fetch(`${this._baseUrl}/cards/likes/${id}`, {
-      method: action == "DELETE" ? "DELETE" : "PUT",
-      headers: this._headers,
-    })
-      .then((res) => {
-        if (!res.ok) {
-          return Promise.reject("Server error");
-        }
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // `${this._baseUrl}/cards/likes/${id}` action == "DELETE" ? "DELETE" : "PUT"
+    return this._transmit(
+      `${this._baseUrl}/cards/likes/${id}`,
+      action == "DELETE" ? "DELETE" : "PUT"
+    );
   }
 
   updateAvatar(data) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        avatar: data.link,
-      }),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          //console.log(res);
-          return Promise.reject("Server error");
-        }
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // `${this._baseUrl}/users/me/avatar` PATCH
+    return this._transmit(`${this._baseUrl}/users/me/avatar`, "PATCH", {
+      avatar: data.link,
+    });
   }
 }
